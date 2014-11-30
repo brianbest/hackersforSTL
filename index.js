e6529780209b32f44a037ef35a846a87969522fd
Brian best <brian.brianb64@gmail.com>
2014-11-30 18:22:09 -0400

    Installing mongo db

--- index.js
+++ index.js
@@ -6,11 +6,73 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
+var util = require('util');
+
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
+
+//----------------------------------------------------
+//Mongo DB application
+//----------------------------------------------------
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+// Use connect method to connect to the Server
+MongoClient.connect("mongodb://localhost:27017/myproject", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+
+
+
+
+  app.post('/', function(req,res){
+    var collection = db.collection('event');
+    var info = {
+      nameEvent : req.body.nameEvent,
+      usrName : req.body.usrName,
+      usrEmail : req.body.usrEmail,
+      evtTime : req.body.evtTime,
+      long : req.body.evtLong,
+      lat: req.body.evtLat
+
+    };
+    collection.insert(info);
+    //Create Event and save to DB
+    console.log('saved event '+ req.body.nameEvent);
+
+  });
+
+  app.post('/getEvents',function(req,res){
+    var collection = db.collection('event');
+    console.log('got request');
+    var stuff =  collection.find().toArray(function(err, items) {
+      console.log(items);
+      console.log(util.inspect(collection, {showHidden: false, depth: null}));
+       return items;
+    });
+    console.log(stuff);
+
+    //
+    //res.send(collection);
+
+    //console.log(collection + 'heres all the events');
+    //collection.find({}).toArray(function (err,docs){
+    //  console.log(docs);
+    //});
+
+  });
+
+  db.close();
+});
+//----------------------------------------------------
+
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/dist/index.html');
@@ -19,14 +81,8 @@ app.get('/', function(req, res){
 app.use(express.static(__dirname + '/dist'));
 
 
-app.post('/create', function(req,res){
 
-  //Create Event and save to DB
-  console.log(req.body.usrName);
 
-  //Join a user to a event table
-
-})
 
 
 http.listen(3000, function(){
